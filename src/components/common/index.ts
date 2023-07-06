8348716d454226fc2f62bbe47af3df9eb82ef7ba
Binh2 <hgqbinh2002@gmail.com>
2023-07-06 19:09:32 +0700

    Add a index file to export other file at the same folder

---
+++ src/components/common/index.ts
@@ -0,0 +1,26 @@
+import { AddInfoButton } from "./AddInfoButton";
+import { AddInfoLink } from "./AddInfoLink";
+import { AppLogo } from "./AppLogo";
+import { AuthGuard } from "./AuthGuard";
+import { CloseButton } from "./CloseButton";
+import { CoverImage } from "./CoverImage";
+import { Header } from "./Header";
+import { Hr } from "./Hr";
+import { Intro } from "./Intro";
+import { LeftSidebar } from "./LeftSidebar";
+import { Loading } from "./Loading";
+import { ProfileImage } from "./ProfileImage";
+import { PublicitySelect } from "./PublicitySelect";
+import { RightSidebar } from "./RightSidebar";
+import { SelectWithIcon } from "./SelectWithicon";
+import { UploadedImage } from "./UploadedImage";
+import { VerticalLine } from "./VerticalLine";
+
+export { 
+  Header,
+
+  AddInfoLink, AddInfoButton, AppLogo, AuthGuard, 
+  CloseButton, CoverImage, Hr, Intro,
+  LeftSidebar, Loading, ProfileImage, PublicitySelect, 
+  RightSidebar, SelectWithIcon, UploadedImage, VerticalLine
+}
