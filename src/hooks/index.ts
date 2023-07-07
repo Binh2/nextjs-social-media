794d459785c2a19321bdea3bda30f903e9c861bd
Binh2 <hgqbinh2002@gmail.com>
2023-07-07 16:49:56 +0700

    exported UploadState

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+import { useCloseAfter } from "./useCloseAfter";
+import { useFadeAfter } from "./useFadeAfter";
+import { UploadState, useUpload } from "./useUpload";
+
+export {
+  useCloseAfter, useFadeAfter, useUpload, UploadState
+}
