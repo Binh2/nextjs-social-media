0bc5581193f1c2ba75815320f8fc7e3f284ed3c5
Binh2 <hgqbinh2002@gmail.com>
2023-07-25 14:59:28 +0700

    Add a className prop

--- src/components/common/CloseButton.tsx
+++ src/components/common/CloseButton.tsx
@@ -4,11 +4,12 @@ import { MouseEventHandler } from "react"
 type Props = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
   size?: number;
+  className?: string
 }
 
-export function CloseButton({ onClick, size=25 }: Props) {
+export function CloseButton({ onClick, size=25, className='' }: Props) {
   return (<>
-    <button className="p-2 mr-2 bg-gray-200 rounded-full" onClick={onClick}>
+    <button className={`p-2 bg-gray-200 rounded-full ${className}`} onClick={onClick}>
       <Image src="/close-icon.svg" alt="Close" width={size} height={size} />
     </button> 
   </>)
