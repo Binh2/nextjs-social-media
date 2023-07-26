e292e2a6be909e427c3bbbee821c8d686b3c7492
Binh2 <hgqbinh2002@gmail.com>
2023-07-26 19:51:42 +0700

    Add margin bottom to Loading

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,10 +1,11 @@
 type Props = {
   width?: number | string,
-  height?: number | string,
+  height?: number | string
+  className?: string
 }
 
-export function Loading({width = 24, height = 24}: Props) {
-  return (<div className={`relative`} style={{width, height}}>
+export function Loading({width = 24, height = 24, className=''}: Props) {
+  return (<div className={`relative ${className}`} style={{width, height}}>
   <div className={`absolute animate-spin border-4 border-solid border-transparent border-t-teal-200 rounded-[100%]`} style={{width, height}}></div>
   <div className={`absolute animate-spin-reverse border-4 border-solid border-transparent border-l-teal-200 rounded-[100%]`} style={{width, height}}></div>
 </div>)
