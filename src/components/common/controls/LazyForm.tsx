c3adee23b17d6c2848ea5f01915215a408c5d1bc
Binh2 <hgqbinh2002@gmail.com>
2023-07-26 14:13:00 +0700

    Add more bottom margin

--- src/components/common/controls/LazyForm.tsx
+++ src/components/common/controls/LazyForm.tsx
@@ -11,7 +11,7 @@ type Props = {
 export function LazyForm({children, show, setShow}: Props) {
   return (<>
     { !show && 
-      <button className={`flex items-center mb-1`} onClick={() => setShow(true)}>
+      <button className={`flex items-center mb-2`} onClick={() => setShow(true)}>
         <PlusCircleIcon className={`text-teal-500 w-5 h-5`} />
         <span className={`text-teal-500 font-bold ml-1 hover:underline`}>Add a university</span>
       </button>
