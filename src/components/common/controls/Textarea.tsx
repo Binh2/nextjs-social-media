dd0bef464e55a69242f822f6493ae53f6f540a3a
Binh2 <hgqbinh2002@gmail.com>
2023-07-26 10:43:53 +0700

    Add a className prop

--- src/components/common/controls/Textarea.tsx
+++ src/components/common/controls/Textarea.tsx
@@ -4,13 +4,14 @@ import { WithLabel } from "./WithLabel";
 type Props = {
   id: string,
   label: string,
+  className?: string
 }
 
-export function Textarea({id, label, ...props}: Props) {
+export function Textarea({id, label, className='', ...props}: Props) {
   const [ focus, setFocus ] = useState(false);
   const [ value, setValue ] = useState('');
   return (<>
-    <WithLabel htmlFor={id} label={label} focus={focus} value={value}>
+    <WithLabel htmlFor={id} label={label} focus={focus} value={value} className={`${className}`}>
       <textarea className={`px-2 outline-none w-full resize-none`} 
         id={id} value={value} {...props} onChange={(e) => setValue(e.target.value)}
         onFocus={() => setFocus(true)} onBlur={() => setFocus(false)}
