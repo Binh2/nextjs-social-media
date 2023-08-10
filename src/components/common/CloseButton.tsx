a14036ca0fe87559dab1b2ee6ae376d5eafa4459
Binh2 <hgqbinh2002@gmail.com>
2023-08-10 21:03:36 +0700

    Add type button so button doesn't submit form

--- src/components/common/CloseButton.tsx
+++ src/components/common/CloseButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export function CloseButton({ onClick, size=25, className='' }: Props) {
   return (<>
-    <button className={`p-2 bg-gray-200 rounded-full ${className}`} onClick={onClick}>
+    <button type="button" className={`p-2 bg-gray-200 rounded-full ${className}`} onClick={onClick}>
       <Image src="/close-icon.svg" alt="Close" width={size} height={size} />
     </button> 
   </>)
