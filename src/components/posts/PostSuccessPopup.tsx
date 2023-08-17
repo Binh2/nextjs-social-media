cb41c972b4ee638843731b2b81916da1111dcbb3
Binh2 <hgqbinh2002@gmail.com>
2023-08-17 07:59:55 +0700

    Use Image instead of img

--- src/components/posts/PostSuccessPopup.tsx
+++ src/components/posts/PostSuccessPopup.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useImperativeHandle, useState } from "react";
 import Popup from "reactjs-popup";
 
@@ -12,7 +13,7 @@ export function PostSuccessPopup({open, setOpen}: Props) {
       contentStyle={{ width: "33%", height: "35%" , borderRadius: "8px", backgroundColor: 'white', borderStyle: "2px solid" , borderColor: "teal-600"  }}
     >
       <div className='flex flex-col items-center justify-center w-full h-full bg-white border-2 border-solid rounded-md border-x-teal-600 ' >
-        <img src='/check-mark-circle-2.svg' alt='check' className='w-32 h-32'></img>
+        <Image src='/check-mark-circle-2.svg' alt='check' className='w-32 h-32' width={0} height={0} unoptimized />
           <span className='text-2xl text-teal-600'>Successful Post</span>
         
       </div>
